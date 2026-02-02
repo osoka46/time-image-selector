16f778f396e58e620eabcc026670d470479c3445
osoka46 <osman.bozdag@yahoo.com>
2026-02-02 18:17:15 +0100

    smoke and regression tags added

--- src/test/java/ImageSelectorTest.java
+++ src/test/java/ImageSelectorTest.java
@@ -1,10 +1,12 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class ImageSelectorTest {
 
     ImageSelector selector = new ImageSelector();
 
+    @Tag("smoke")
     @Test
     public void testMorning() {
         // Happy Path
@@ -13,21 +15,25 @@ public class ImageSelectorTest {
         Assertions.assertEquals("morning.jpg", selector.selectImage("07:00"));
     }
 
+    @Tag("regression")
     @Test
     public void testNoon() {
         Assertions.assertEquals("noon.jpg", selector.selectImage("13:00"));
     }
 
+    @Tag("regression")
     @Test
     public void testAfternoon() {
         Assertions.assertEquals("afternoon.jpg", selector.selectImage("15:00"));
     }
 
+    @Tag("smoke")
     @Test
     public void testEvening() {
         Assertions.assertEquals("evening.jpg", selector.selectImage("21:00"));
     }
 
+    @Tag("regression")
     @Test
     public void testNight() {
         // before night
@@ -36,11 +42,8 @@ public class ImageSelectorTest {
         Assertions.assertEquals("night.jpg", selector.selectImage("02:00"));
     }
 
-    @Test
-    public void testToFail() {
-        //  Assertions.assertEquals("night.jpgTest", selector.selectImage("23:00"));
-    }
 
+    @Tag("regression")
     @Test
     public void testInvalidInput() {
         Assertions.assertEquals("broken_watch.jpg", selector.selectImage("potato"));
